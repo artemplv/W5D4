8d4e2d4ebd5bb03d54faa49c345e7348535b112c
jeffreyferrert <jeffrey.ferrert@gmail.com>
2023-06-01 15:43:07 -0400

    Course prerequisite

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,4 +10,17 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many :enrollments,
+        class_name: 'Enrollment',
+        foreign_key: :course_id,
+        primary_key: :id
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+    belongs_to :prerequisite,
+        class_name: 'Course',
+        foreign_key: :prereq_id,
+        primary_key: :id
 end
