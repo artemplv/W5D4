302ba60f3d4de89b72910a38ccbcbe051f7b7d67
artemplv <artemplv@gmail.com>
2023-06-01 16:12:53 -0400

    add instructor association

--- app/models/course.rb
+++ app/models/course.rb
@@ -22,5 +22,11 @@ class Course < ApplicationRecord
     belongs_to :prerequisite,
         class_name: 'Course',
         foreign_key: :prereq_id,
-        primary_key: :id
+        primary_key: :id,
+        optional: true
+
+    belongs_to :instructor,
+    class_name: 'User',
+    foreign_key: :instructor_id,
+    primary_key: :id
 end
